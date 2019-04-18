c9d582e247b1cd519e3f9564f371bda41b867c81
zysite <zysite@outlook.com>
2019-04-18 14:58:41 +0800

    Add char lstm

--- parser/modules/__init__.py
+++ parser/modules/__init__.py
@@ -2,8 +2,10 @@
 
 from .biaffine import Biaffine
 from .bilstm import BiLSTM
+from .char_lstm import CHAR_LSTM
 from .dropout import IndependentDropout, SharedDropout
 from .mlp import MLP
 
 
-__all__ = ['MLP', 'Biaffine', 'BiLSTM', 'IndependentDropout', 'SharedDropout']
+__all__ = ['CHAR_LSTM', 'MLP', 'Biaffine', 'BiLSTM',
+           'IndependentDropout', 'SharedDropout']
